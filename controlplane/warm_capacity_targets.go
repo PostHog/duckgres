fea8ac75a9dfeb46b37753c587b58ab90625af0a
Bill Guowei Yang <bill@posthog.com>
2026-05-21 17:03:16 -0400

    Fix warm capacity helper build tags

--- controlplane/warm_capacity_targets.go
+++ controlplane/warm_capacity_targets.go
@@ -15,31 +15,6 @@ type dynamicWarmCapacityConfig struct {
 	MaxWorkers          int
 }
 
-func dynamicWarmCapacityConfigFromK8s(cfg K8sConfig) dynamicWarmCapacityConfig {
-	return dynamicWarmCapacityConfig{
-		Enabled:             cfg.DynamicWarmCapacityEnabled,
-		MissesPerWorker:     cfg.WarmCapacityMissesPerWorker,
-		DynamicImageCeiling: cfg.WarmCapacityDynamicImageCeiling,
-		DynamicTotalCeiling: cfg.WarmCapacityDynamicTotalCeiling,
-		MaxWorkers:          cfg.MaxWorkers,
-	}
-}
-
-func computeBaseWarmCapacityTargets(defaultImage string, sharedWarmTarget int, perImageFloors map[string]int) map[string]int {
-	out := sanitizeWarmCapacityTargets(perImageFloors)
-	defaultImage = strings.TrimSpace(defaultImage)
-	if defaultImage == "" {
-		return out
-	}
-	if sharedWarmTarget < 0 {
-		sharedWarmTarget = 0
-	}
-	if sharedWarmTarget > out[defaultImage] {
-		out[defaultImage] = sharedWarmTarget
-	}
-	return out
-}
-
 func computeDynamicWarmCapacityTargets(baseTargets map[string]int, aggregates []configstore.WarmCapacityMissAggregate, cfg dynamicWarmCapacityConfig) map[string]int {
 	targets := sanitizeWarmCapacityTargets(baseTargets)
 	if !cfg.Enabled {
